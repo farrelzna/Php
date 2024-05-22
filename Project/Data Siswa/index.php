08c6e6220fc106c94f5f914515b20a36239daa3c
farrelzna <farrelizzanmaulana@smkwikrama.sch.id>
2024-05-22 22:00:48 +0700

    Project3 > 22-05-2024

--- Project/Data Siswa/index.php
+++ Project/Data Siswa/index.php
@@ -1,98 +1,93 @@
+<?php
+session_start();
+
+if(isset($_POST['hapus'])){
+    if(isset($_POST['hapus_key'])){
+        $key = $_POST['hapus_key']; // Mengambil kunci dari form
+        unset($_SESSION['dataSiswa'][$key]); // Menghapus data sesuai kunci
+        header('Location: ' . $_SERVER['PHP_SELF']); // Redirect kembali ke halaman ini setelah penghapusan
+        exit();
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <title>Home</title>
-</head>
-<style>
-    a {
-        text-decoration: none;
-        color: white;
-    }
-</style>
-<body>
-    <div class="container mt-5 pt-5">
-        <div class="row">
-            <div class="col-12 col-sm-8 col-md-6 m-auto">
-                <div class="card text-center">
-                    <div class="card-body">
-                        <h1>DATA SISWA</h1>
-                        <form action="" method="POST" class="row d-flex align-items-center">
-                            <label for="nama">Nama Siswa</label>
-                            <input type="text" id="nama" placeholder="masukan nama siswa" name="nama">
-
-                            <label for="nis">NIS Siswa</label>
-                            <input type="number" id="nis" placeholder="masukan nis siswa" name="nis">
-
-                            <label for="rayon">Rayon</label>
-                            <input type="text" id="rayon" placeholder="masukan rayon siswa" name="rayon">
-
-                            <div class="col mt-3">
-                                <button class="btn btn-primary" type="submit" name="kirim">
-                                    <i class='bx bx-plus'></i>Tambah</button>
-                                <button class="btn btn-danger" type="submit" name="cetak">
-                                    <i class='bx bx-printer'></i>print</button>
-                                <button class="btn btn-secondary" type="submit" name="cetak">
-                                    <a href="destroy.php">Reset</a></button>
-                            </div>
-                        </form>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <div id="container"></div>
+            <div id="section1">
+                <h2>Masukan Data</h2>
+                    <hr>
+                <form action="" method="post">  
+                    <div class="nama" id="form-control">
+                        <input class="input input-alt" placeholder="Nama" required="" type="text" name="nama" id="nama">
+                            <span class="input-border input-border-alt"></span>
                     </div>
-                </div>
+                        <br><br>
+                    <div class="nis" id="form-control">
+                        <input class="input input-alt" placeholder="Nis" required="" type="number" name="nis" id="nis">
+                            <span class="input-border input-border-alt"></span>
+                    </div>
+                        <br><br>
+                    <div class="rayon" id="form-control">
+                        <input class="input input-alt" placeholder="Rayon" required="" type="text" name="rayon" id="rayon">
+                    </div>
+                        <br><br>
+                    <button type="submit" name="tmb">Submit</button>
+                </form>
+            </div>
+                <br><br>
+            <div id="section2">
+                <h2>Daftar Siswa</h2>
+                    <hr>
+                <div class="siswa-list">
+                    
+                    <?php
+                    
+                        if(!isset($_SESSION['dataSiswa'])){
+                            $_SESSION['dataSiswa'] = array();
+                        }
+
+                        if(isset($_POST['tmb'])){
+                            if($_POST["nama"] == "" || $_POST["nis"] == "" || $_POST['rayon'] == ""){
+                                echo "Data kosong";
+                            }else{
+                                $siswa = array(
+                                    "nama" => $_POST['nama'],
+                                    "nis" => $_POST['nis'],
+                                    "rayon" => $_POST['rayon']
+                                );
+                                array_push($_SESSION['dataSiswa'], $siswa);
+                            }
+                        }
+                                    
+                    ?>
+                    <?php if (!empty($_SESSION['dataSiswa'])): ?>
+                    <?php foreach ($_SESSION['dataSiswa'] as $key => $value): ?>
+                        <div class="section">
+                            <p><?php echo htmlspecialchars($value['nama']); ?> - <?php echo htmlspecialchars($value['nis']); ?> - <?php echo htmlspecialchars($value['rayon']); ?></p>
+                            <form method="post" style="display:inline;">
+                                <input type="hidden" name="hapus_key" value="<?php echo $key; ?>">
+                                <button class="button2" type="submit" name="hapus">Hapus</button>
+                            </form>
+                            <form method="get" action="updata.php" style="display:inline;">
+                                <input type="hidden" name="key" value="<?php echo $key; ?>">
+                                <button class="button2" type="submit" class="ubah-button">Ubah</button>
+                            </form>
+                        </div>
+                            <br>
+                    <?php endforeach; ?>
+                    <?php else: ?>
+                        <p>Tidak ada data siswa.</p>
+                    <?php endif;?>
+
             </div>
         </div>
-    </div>
-    
-
-    <?php
-        session_start();
-
-        // initialize an based Array if the arrays is not initialized yet
-        if (!isset($_SESSION['dataSiswa'])) {
-            $_SESSION['dataSiswa'] = array();
-        }
-
-        if (isset($_POST["kirim"])) {
-            if ($_POST['nama'] == "" && $_POST['nis'] == "" && $_POST['rayon'] == "") {
-                echo "data kosong <br>";
-            } else {
-                $siswa = array(
-                    "nama" => $_POST['nama'],
-                    "nis" => $_POST['nis'],
-                    "rayon" => $_POST['rayon']
-                );
-
-                array_push($_SESSION['dataSiswa'], $siswa);
-            }
-        }
-
-        if (!empty($_SESSION['dataSiswa'])) {
-            foreach ($_SESSION['dataSiswa'] as $key => $value) {
-                echo $value["nama"] . " ";
-                echo $value["nis"] . " ";
-                echo $value["rayon"] . "";
-                echo "<a href='?hapus=".$key. "' class=\"btn btn-danger ms-2\">Hapus</a>";
-                echo "<br>";
-            }
-        }
-
-        if (isset($_GET['hapus'])) {
-                $key = $_GET['hapus'];
-                unset($_SESSION['dataSiswa'][$key]);
-
-                header('location: ' . $_SERVER['PHP_SELF']);
-                exit;
-        }
-
-    ?>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-</body>
-
+    </body>
 </html>
